4daa917196c2b71caada094e43d74943ed268fa6
Mary Jenn <mary@maryfjenn.com>
2013-01-31 19:47:16 -0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lexicon::Application.config.secret_token = '56cf2b709ae5bd31109c4ef72b7bb50306fe0e429db88b46868fc1a4cfd352f6e893a96472c279a2f95a960a11a9d0efcc464bdc3d59c9b62a1e3ff17a188d85'
