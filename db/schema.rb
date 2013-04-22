ab7355680ccfa0799bfa663927b70df71d0f584b
Mary Jenn <maryjenn@ENG001067.local>
2013-04-22 10:51:39 -0700

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130203051804) do
+ActiveRecord::Schema.define(:version => 20130203014425) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(:version => 20130203051804) do
     t.string   "synonyms"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "user_id"
   end
 
 end
